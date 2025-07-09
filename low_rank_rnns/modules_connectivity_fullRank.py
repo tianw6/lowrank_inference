276f2fcfe0d0e064e442618d28d6d1a1edb111dc
tianw6 <protoss0318@gmail.com>
2025-07-09 18:24:24 -0400

    add debugLowRank, might combined 3-area rnn with low rank. Will check later

--- low_rank_rnns/modules_connectivity_fullRank.py
+++ low_rank_rnns/modules_connectivity_fullRank.py
@@ -118,8 +118,7 @@ def train(net, _input, _target, _mask, n_epochs, lr=1e-2, batch_size=32, plot_le
 
             ################################## Tian changed this
             # add L2 regularization 
-            w_rec_eff = torch.abs(net.wrec) * net.wrec_mask
-            loss += 0.001*torch.norm(w_rec_eff, p=2)/np.sqrt(torch.numel(w_rec_eff)) + 0.001*torch.norm(h, p=2)/np.sqrt(torch.numel(h))
+            loss += 0.001*torch.norm(net.w_rec_eff, p=2)/np.sqrt(torch.numel(net.w_rec_eff)) + 0.001*torch.norm(h, p=2)/np.sqrt(torch.numel(h))
             ##################################
 
             losses.append(loss.item())
@@ -398,3 +397,268 @@ class FullRankRNN(nn.Module):  # TODO rename biases train_biases, add to cloning
         return new_net
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+class LowRankRNN(nn.Module):
+    """
+    This class implements the low-rank RNN. Instead of being parametrized by an NxN connectivity matrix, it is
+    parametrized by two Nxr matrices m and n such that the connectivity is m * n^T
+    """
+
+    def __init__(self, input_size, hidden_size, output_size, noise_std, alpha, rank=1, train_m = True,
+                 train_wi=False, train_wo=False, train_wrec=True, train_h0=False, train_si=True, train_so=True,
+                 wi_init=None, wo_init=None, m_init=None, n_init=None, wrec_mask = None, wi_mask = None, wo_mask = None, si_init=None, so_init=None, h0_init=None,
+                 add_biases=False, non_linearity=torch.relu, output_non_linearity=torch.relu):
+        """
+        :param input_size: int
+        :param hidden_size: int
+        :param output_size: int
+        :param noise_std: float
+        :param alpha: float, value of dt/tau
+        :param rank: int
+        :param train_wi: bool
+        :param train_wo: bool
+        :param train_wrec: bool
+        :param train_h0: bool
+        :param train_si: bool
+        :param train_so: bool
+        :param wi_init: torch tensor of shape (input_dim, hidden_size)
+        :param wo_init: torch tensor of shape (hidden_size, output_dim)
+        :param m_init: torch tensor of shape (hidden_size, rank)
+        :param n_init: torch tensor of shape (hidden_size, rank)
+        :param si_init: torch tensor of shape (input_size)
+        :param so_init: torch tensor of shape (output_size)
+        :param h0_init: torch tensor of shape (hidden_size)
+        """
+        super(LowRankRNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.noise_std = noise_std
+        self.alpha = alpha
+        self.rank = rank
+        self.train_m = train_m
+        self.train_wi = train_wi
+        self.train_wo = train_wo
+        self.train_wrec = train_wrec
+        self.train_h0 = train_h0
+        self.train_si = train_si
+        self.train_so = train_so
+        self.non_linearity = non_linearity
+        self.output_non_linearity = output_non_linearity
+
+
+        self.wrec_mask = nn.Parameter(torch.Tensor(hidden_size, hidden_size), requires_grad=False)
+        self.wi_mask = nn.Parameter(torch.Tensor(input_size, hidden_size), requires_grad=False)
+        self.wo_mask = nn.Parameter(torch.Tensor(hidden_size, output_size), requires_grad=False)
+
+        # Define parameters
+        self.wi = nn.Parameter(torch.Tensor(input_size, hidden_size))
+        self.si = nn.Parameter(torch.Tensor(input_size))
+
+        if train_wi:
+            self.si.requires_grad = False
+        else:
+            self.wi.requires_grad = False
+        if not train_si:
+            self.si.requires_grad = False
+        self.m = nn.Parameter(torch.Tensor(hidden_size, rank))
+        self.n = nn.Parameter(torch.Tensor(hidden_size, rank))
+
+        if not train_m:
+            self.m.requires_grad = False
+            
+        if not train_wrec:
+            self.m.requires_grad = False
+            self.n.requires_grad = False
+        self.b = nn.Parameter(torch.Tensor(hidden_size))
+        if not add_biases:
+            self.b.requires_grad = False
+        self.wo = nn.Parameter(torch.Tensor(hidden_size, output_size))
+        self.so = nn.Parameter(torch.Tensor(output_size))
+        if train_wo:
+            self.so.requires_grad = False
+        if not train_wo:
+            self.wo.requires_grad = False
+        if not train_so:
+            self.so.requires_grad = False
+        self.h0 = nn.Parameter(torch.Tensor(hidden_size))
+        if not train_h0:
+            self.h0.requires_grad = False
+
+        # Initialize parameters
+        with torch.no_grad():
+            if wi_init is None:
+                self.wi.normal_()
+            else:
+                self.wi.copy_(wi_init)
+            if si_init is None:
+                self.si.set_(torch.ones_like(self.si))
+            else:
+                self.si.copy_(si_init)
+            if m_init is None:
+                self.m.normal_()
+            else:
+                self.m.copy_(m_init)
+            if n_init is None:
+                self.n.normal_()
+            else:
+                self.n.copy_(n_init)
+            self.b.zero_()     # TODO add biases initializer
+            if wo_init is None:
+                self.wo.normal_(std=4.)
+            else:
+                self.wo.copy_(wo_init)
+            if so_init is None:
+                self.so.set_(torch.ones_like(self.so))
+            else:
+                self.so.copy_(so_init)
+            if h0_init is None:
+                self.h0.zero_()
+            else:
+                self.h0.copy_(h0_init)
+
+
+
+            ############### define masks
+            if wi_mask is not None:
+                self.wi_mask.copy_(wi_mask)  
+            else:
+                self.wi_mask.set_(torch.ones_like(self.wi_mask))
+
+            if wo_mask is not None:
+                self.wo_mask.copy_(wo_mask)
+            else: 
+                self.wo_mask.set_(torch.ones_like(self.wo_mask))      
+
+            if wrec_mask is not None:
+                self.wrec_mask.copy_(wrec_mask)
+            else: 
+                self.wrec_mask.set_(torch.ones_like(self.wrec_mask))                     
+            ###############        
+
+
+        self.wrec, self.wi_full, self.wo_full = [None] * 3
+        self._define_proxy_parameters()
+
+    def _define_proxy_parameters(self):
+        self.wrec = None   # For optimization purposes the full connectivity matrix is never computed explicitly
+        self.wi_full = (self.wi.t() * self.si).t()
+        self.wo_full = self.wo * self.so
+
+
+
+    def effective_weight(self, n, m, mask, w_fix=0):
+        """ compute the effective weight """
+    
+        w_eff = n.matmul(m.t()) + w_fix
+
+        return w_eff
+
+    def effective_IO_weight(self, w, mask):
+        
+        w_eff = w*mask
+
+        return w_eff  
+
+
+    def forward(self, input, return_dynamics=True, initial_states=None):
+        """
+        :param input: tensor of shape (batch_size, #timesteps, input_dimension)
+        Important: the 3 dimensions need to be present, even if they are of size 1.
+        :param return_dynamics: boolean
+        :return: if return_dynamics=False, output tensor of shape (batch_size, #timesteps, output_dimension)
+                 if return_dynamics=True, (output tensor, trajectories tensor of shape (batch_size, #timesteps, #hidden_units))
+        """
+        batch_size = input.shape[0]
+        seq_len = input.shape[1]
+        if initial_states is None:
+            initial_states = self.h0
+        h = initial_states.clone()
+        r = self.non_linearity(h)
+        self._define_proxy_parameters()
+        noise = torch.randn(batch_size, seq_len, self.hidden_size, device=self.m.device)
+        output = torch.zeros(batch_size, seq_len, self.output_size, device=self.m.device)
+        if return_dynamics:
+            trajectories = torch.zeros(batch_size, seq_len + 1, self.hidden_size, device=self.m.device)
+            trajectories[:, 0, :] = h
+
+        # simulation loop
+        for i in range(seq_len):
+
+
+
+            # compute effective weight
+            self.w_rec_eff = self.effective_weight(n=self.n, m = self.m, mask=self.wrec_mask)
+            self.w_in_eff = self.effective_IO_weight(w=self.wi_full, mask=self.wi_mask)
+            self.w_out_eff = self.effective_IO_weight(w=self.wo_full, mask=self.wo_mask)
+
+
+
+            h = h + self.noise_std * noise[:, i, :] + self.alpha * \
+                (-h + r.matmul(self.n).matmul(self.m.t()) / self.hidden_size +
+                    input[:, i, :].matmul(self.w_in_eff) + self.b)
+
+            # h = h + self.noise_std * noise[:, i, :] + self.alpha * \
+            #     (-h + r.matmul(self.w_rec_eff) / self.hidden_size +
+            #         input[:, i, :].matmul(self.w_in_eff) + self.b)
+
+
+
+            r = self.non_linearity(h)
+            output[:, i, :] = h @ (self.w_out_eff) / self.hidden_size
+
+            if return_dynamics:
+                trajectories[:, i + 1, :] = h
+
+
+
+        if not return_dynamics:
+            return output
+        else:
+            return output, trajectories
+
+
+
+
+
+    def clone(self):
+
+
+        new_net = LowRankRNN(self.input_size, self.hidden_size, self.output_size, self.noise_std, self.alpha,
+                             self.rank, self.train_m, self.train_wi, self.train_wo, self.train_wrec, self.train_h0, self.train_si,
+                             self.train_so, self.wi, self.wo, self.m, self.n, self.wrec_mask, self.wi_mask, self.wo_mask, self.si, self.so, self.h0, False,
+                             self.non_linearity, self.output_non_linearity)
+        new_net._define_proxy_parameters()
+        return new_net
+
+    def load_state_dict(self, state_dict, strict=True):
+        """
+        override
+        """
+        if 'rec_noise' in state_dict:
+            del state_dict['rec_noise']
+        super().load_state_dict(state_dict, strict)
+        self._define_proxy_parameters()
+
+    def svd_reparametrization(self):
+        """
+        Orthogonalize m and n via SVD
+        """
+        with torch.no_grad():
+            structure = (self.m @ self.n.t()).numpy()
+            m, s, n = np.linalg.svd(structure, full_matrices=False)
+            m, s, n = m[:, :self.rank], s[:self.rank], n[:self.rank, :]
+            self.m.set_(torch.from_numpy(m * np.sqrt(s)))
+            self.n.set_(torch.from_numpy(n.transpose() * np.sqrt(s)))
+            self._define_proxy_parameters()
